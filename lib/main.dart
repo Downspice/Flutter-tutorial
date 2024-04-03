f09042fed44bde8ee9c80196f8888705add427de
Awer Joseph Kweku <Josephaw@stlghana.com>
2024-04-03 12:01:25 +0100

    revert

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
-import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,99 +13,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: CameraScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'Camera App',
+      home: Camera(),
     );
   }
 }
 
-class CameraScreen extends StatefulWidget {
-  const CameraScreen({super.key});
+class Camera extends StatefulWidget {
+  const Camera({super.key});
 
   @override
-  _CameraScreenState createState() => _CameraScreenState();
+  State<Camera> createState() => _CameraState();
 }
 
-class _CameraScreenState extends State<CameraScreen> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-  late FaceDetector _faceDetector;
+class _CameraState extends State<Camera> {
+  File? _image;
+  final picker = ImagePicker();
 
-  @override
-  void initState() {
-    super.initState();
-    _initializeCamera();
-    _initializeFaceDetector();
-  }
-
-  Future<void> _initializeCamera() async {
-    final cameras = await availableCameras();
-    final firstCamera = cameras.first;
-
-    _controller = CameraController(
-      firstCamera,
-      ResolutionPreset.medium,
-    );
-
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  Future<void> _initializeFaceDetector() async {
-    _faceDetector = GoogleMlKit.vision.faceDetector(FaceDetectorOptions(
-      enableClassification: true,
-      enableTracking: true,
-      enableContours: true,
-    ));
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    _faceDetector.close();
-    super.dispose();
-  }
-
-  void _capturePicture() async {
-    try {
-      await _initializeControllerFuture;
-      final image = await _controller.takePicture();
-
-      // Detect faces in the captured image
-      final inputImage = InputImage.fromFilePath(image.path);
-      final faces = await _faceDetector.processImage(inputImage);
-
-      if (faces.isNotEmpty) {
-        print('Face detected! Capturing picture...');
-        // Do something with the captured image
-        print('Picture captured: ${image.path}');
+  Future getImage() async {
+    final pickedImage = await picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedImage != null) {
+        _image = File(pickedImage.path);
       } else {
-        print('No face detected.');
+        print("No image");
       }
-    } catch (e) {
-      print('Error taking picture: $e');
-    }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Camera Example'),
+        title: const Text("Camera"),
+        backgroundColor: Colors.amber,
       ),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller);
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
+      body: Center(
+        child: _image == null
+            ? const Text("No captured photos")
+            : Image.file(_image!),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _capturePicture,
-        child: Icon(Icons.camera),
+        onPressed: getImage,
+        child: const Icon(Icons.add_a_photo),
+        backgroundColor: Colors.amber,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
